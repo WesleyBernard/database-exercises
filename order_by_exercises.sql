5be8f1706c1a4b1b6cb35b8deac4bb3510b4f0b0
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-29 15:10:32 -0400

    completing order exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,30 @@
+select *
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by last_name, first_name;
+
+select *
+from employees
+where  last_name like '%E%'
+order by emp_no desc ;
+
+select *
+from employees
+where  last_name like '%q%';
+
+select *
+from employees
+where (first_name = 'Irena'
+    or first_name = 'Vidya'
+    or first_name = 'Maya')
+  and gender = 'M';
+
+select *
+from employees
+where  left(last_name, 1) = 'E'
+  and right(last_name, 1) = 'e';
+
+select *
+from employees
+where  last_name like '%q%'
+  and last_name not like '%qu%'
