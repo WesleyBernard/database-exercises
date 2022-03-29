1eb3821bb8fc2f36c90d781f774ecb526b20acba
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-29 11:43:00 -0400

    completing where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,3 +9,20 @@ where  left(last_name, 1) = 'E';
 select *
 from employees
 where  last_name like '%q%';
+
+select *
+from employees
+where (first_name = 'Irena'
+or first_name = 'Vidya'
+or first_name = 'Maya')
+and gender = 'M';
+
+select *
+from employees
+where  left(last_name, 1) = 'E'
+and right(last_name, 1) = 'e';
+
+select *
+from employees
+where  last_name like '%q%'
+and last_name not like '%qu%'
